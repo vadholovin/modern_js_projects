f0bc53e3f34f11da355304c9ca811f08a1665c0c
Vadym Holovin <holyvad@gmail.com>
2018-01-21 16:56:11 +0300

    Added ability to remove a task, clear all tasks and filter them

--- 01_task_list/app.js
+++ 01_task_list/app.js
@@ -1,52 +1,93 @@
 // Define UI variables
 const form = document.querySelector('#task-form');
-const taskList = document.querySelector('.collection');
-const clearBtn = document.querySelector('.clear-tasks');
 const taskInput = document.querySelector('#task');
-const filter = document.querySelector('#filter');
+const taskList = document.querySelector('.collection');
+const filter = document.querySelector('#filter');;
+const clearBtn = document.querySelector('.clear-tasks');
 
-// load all event listeners
+// Load all event listeners
 loadEventListeners();
 
-// load all event listeners
+// Load all event listeners
 function loadEventListeners() {
-  // add task event
+  // Add task event
   form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+  // Clear tasks event
+  clearBtn.addEventListener('click', clearTasks);
+  // Filter tasks
+  filter.addEventListener('keyup', filterTasks);
 }
 
 // Add task
 function addTask(e) {
-  // Prevent adding empty entry
-  if (taskInput.value === '') {
+  if (!taskInput.value) {
     alert('Add a task');
   } else {
     createAndAddTask();
   }
 
-  e.preventDefault();
-
   function createAndAddTask() {
-    // Create li element
     const li = document.createElement('li');
-    // Add class
+    const taskText = taskInput.value;
+    const deleteBtn = document.createElement('a');
+    
+    // Add attr, class and icon to delete button
+    deleteBtn.setAttribute('href', '#');
+    deleteBtn.className = 'delete-item secondary-content';
+    deleteBtn.innerHTML = '<i class="fa fa-remove"></i>';
+
+    // Add class to li
     li.className = 'collection-item';
-    // Create text node and append to li
-    li.appendChild(document.createTextNode(taskInput.value));
-    // Create link
-    const link = document.createElement('a');
-    // Set attribute href
-    link.setAttribute('href', '#');
-    // Add class
-    link.className = 'delete-item secondary-content';
-    // Add icon
-    link.innerHTML = '<i class="fa fa-remove"></li>';
-    // Append the link to li
-    li.appendChild(link);
+    // Append task text and delete button to li
+    li.appendChild(document.createTextNode(taskText));
+    li.appendChild(deleteBtn);
 
     // Append li to ul
     taskList.appendChild(li);
 
-    // Clear input
+    // Clear task input
     taskInput.value = '';
   }
+
+  e.preventDefault();
+}
+
+// Remove task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    if (confirm('Remove this task?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+
+  e.preventDefault();
+}
+
+// Clear tasks
+function clearTasks(e) {
+  if (confirm('Remove all tasks?')) {
+    while (taskList.firstChild) {
+      taskList.firstChild.remove();
+    }
+  }
+
+  e.preventDefault();
+}
+
+// Filter tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+  const lisList = document.querySelectorAll('.collection-item');
+
+  lisList.forEach(function(task) {
+    const taskText = task.firstChild.textContent;
+
+    if (taskText.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
