0629d4361808adeb366c1314fd4ff24f95e00319
Vadym Holovin <holyvad@gmail.com>
2018-01-27 15:07:46 +0300

    Number guesser: Created UI

---
+++ 03_number_guesser/app.js
@@ -0,0 +1,8 @@
+/*
+GAME FUNCTION:
+- Player must guess number between a min and max
+- Player gets a cirtain amount of guesses
+- Notify player of guesses remaining
+- Notify player of the correct answer if loose
+- Let player choose to play again
+*/
