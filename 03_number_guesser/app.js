0fc40ac5ad7a2006d853138d874acfba48ca9679
Vadym Holovin <holyvad@gmail.com>
2018-01-27 16:04:47 +0300

    Number guesser: Added validation and winning case

--- 03_number_guesser/app.js
+++ 03_number_guesser/app.js
@@ -6,3 +6,46 @@ GAME FUNCTION:
 - Notify player of the correct answer if loose
 - Let player choose to play again
 */
+
+// Game values
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+// UI Elements
+const game = document.querySelector('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessInput = document.querySelector('#guess-input'),
+      guessBtn = document.querySelector('#guess-btn'),
+      message = document.querySelector('.message');
+
+// Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+guessBtn.addEventListener('click', function() {
+  let guess = parseInt(guessInput.value);
+
+  // Validate
+  if (isNaN(guess) || guess < min || guess > max) {
+    setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+  }
+
+  // Check if won
+  if (guess == winningNum) {
+    // Disable input
+    guessInput.disabled = true;
+    // Change border color
+    guessInput.style.borderColor = 'green';
+    // Set message
+    setMessage(`${winningNum} is correct, YOU WIN BRO!`, 'green');
+  }
+});
+
+// Set message
+function setMessage(text, color) {
+  message.style.color = color;
+  message.textContent = text;
+}
