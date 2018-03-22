9495401fb6fe87e2abc671e8ac297ae3a22e3623
Vadym Holovin <holyvad@gmail.com>
2018-03-22 13:59:58 +0300

    Added show repos

--- 05_github_finder/github.js
+++ 05_github_finder/github.js
@@ -2,15 +2,21 @@ class GitHub {
   constructor() {
     this.client_id = '9b9f7fdda99af83c583a';
     this.client_secret = '629ecfc9fbea26029da31d1a0cdac9b55c14fa75';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     };
   }
 }
