d473e559d87b736b5e0e461c446d29b3e87235b7
Vadym Holovin <holyvad@gmail.com>
2018-03-22 12:04:04 +0300

    Added showing profile

--- 05_github_finder/app.js
+++ 05_github_finder/app.js
@@ -1,6 +1,9 @@
 // Init GitHub
 const github = new GitHub();
 
+// Init UI
+const ui = new UI();
+
 // Search input
 const searchUser = document.getElementById("searchUser");
 
@@ -18,7 +21,7 @@ searchUser.addEventListener('keyup', (e) => {
 
         } else {
           // Show profile
-           
+          ui.showProfile(data.profile);
         }
       });
   } else {
