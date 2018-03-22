52b8c80b3f58ca490c86dafb9f4399c1212e1c32
Vadym Holovin <holyvad@gmail.com>
2018-03-22 12:44:26 +0300

    Added alert message to ui.js

--- 05_github_finder/app.js
+++ 05_github_finder/app.js
@@ -18,7 +18,7 @@ searchUser.addEventListener('keyup', (e) => {
       .then(data => {
         if (data.profile.message == 'Not Found') {
           // Show alert
-
+          ui.showAlert('User not found', 'alert alert-danger');
         } else {
           // Show profile
           ui.showProfile(data.profile);
@@ -26,6 +26,6 @@ searchUser.addEventListener('keyup', (e) => {
       });
   } else {
     // Clear profile
-
+    ui.clearProfile();
   }
 });
